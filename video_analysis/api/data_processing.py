fix: eval metric calculation per group

video_analysis/api/data_processing.py
@@ -1,13 +1,22 @@
-import pandas as pd
-from sklearn.decomposition import PCA
-from video_analysis.api.entities.group_statistics.models import GroupStatisticsModel, EvalMetricsModel, GroupCategoryEnum
-from video_analysis.api.entities.video.models import VideoModel
-from sklearn.metrics import precision_score, recall_score, f1_score
-import numpy as np
-from scipy.spatial.distance import pdist
 from typing import Union
 
-def process_data(video_df: pd.DataFrame) -> dict[str, Union[list[GroupStatisticsModel], list[VideoModel]]]:
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import pdist
+from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report
+
+from video_analysis.api.entities.group_statistics.models import (
+    EvalMetricsModel,
+    GroupCategoryEnum,
+    GroupStatisticsModel,
+)
+from video_analysis.api.entities.video.models import VideoModel
+
+
+def process_data(
+    video_df: pd.DataFrame,
+) -> dict[str, Union[list[GroupStatisticsModel], list[VideoModel]]]:
     """Processes a dataframe containing video information, calculating group statistics and video information.
 
     Args:
@@ -20,12 +29,15 @@ def process_data(video_df: pd.DataFrame) -> dict[str, Union[list[GroupStatistics
     """
     processed_df = video_df.copy()
     # Convert feature vector string column to actual float arrays
-    processed_df["feature_vector"] = list(np.array(processed_df["feature_vector"].str.split(", ").to_list(), dtype=float))
+    processed_df["feature_vector"] = list(
+        np.array(processed_df["feature_vector"].str.split(", ").to_list(), dtype=float)
+    )
 
     group_stats = get_video_groups_stats(processed_df)
     videos = get_videos(processed_df)
 
     return {"group_stats": group_stats, "videos": videos}
+
 
 def get_videos(video_df: pd.DataFrame) -> list[VideoModel]:
     """Obtains a list with the video information present in the given DataFrame, modified to include numerical feature vectors and addicional PCA components
@@ -37,11 +49,24 @@ def get_videos(video_df: pd.DataFrame) -> list[VideoModel]:
         list[VideoModel]: The list with the video information.
     """
     processed_df = video_df.copy()
-    processed_df.rename({"actual_label": "topic", "predicted_label": "predicted_topic", "tvshow": "tv_show"}, axis=1, inplace=True)
+    processed_df.rename(
+        {
+            "actual_label": "topic",
+            "predicted_label": "predicted_topic",
+            "tvshow": "tv_show",
+        },
+        axis=1,
+        inplace=True,
+    )
     video_records = processed_df.to_dict(orient="records")
-    pca_res = PCA(n_components=2).fit_transform(np.array(processed_df["feature_vector"].to_list()))
+    pca_res = PCA(n_components=2).fit_transform(
+        np.array(processed_df["feature_vector"].to_list())
+    )
 
-    return [VideoModel(**video, pca_x=pca_res[idx, 0], pca_y=pca_res[idx, 1]) for idx, video in enumerate(video_records)]
+    return [
+        VideoModel(**video, pca_x=pca_res[idx, 0], pca_y=pca_res[idx, 1])
+        for idx, video in enumerate(video_records)
+    ]
 
 
 def get_video_groups_stats(video_df: pd.DataFrame) -> list[GroupStatisticsModel]:
@@ -55,42 +80,86 @@ def get_video_groups_stats(video_df: pd.DataFrame) -> list[GroupStatisticsModel]
         list[GroupStatisticsModel]: List with the groups' statistical information.
     """
     groups: list[GroupStatisticsModel] = []
-    video_type_list = video_df["actual_label"].unique()
+    video_topic_list = video_df["actual_label"].unique()
 
-    # Calculate metrics and similarity for entire set
-    general_eval_metrics = calc_eval_metrics(video_df, labels=video_type_list)
-    general_avg_cosine_distance = pdist(video_df["feature_vector"].to_list(), metric="cosine").mean()
-    
-    groups.append(GroupStatisticsModel(name="all", category=GroupCategoryEnum.all, eval_metrics=general_eval_metrics, avg_cosine_intra_similarity=general_avg_cosine_distance))
+    report = classification_report(
+        video_df["actual_label"],
+        video_df["predicted_label"],
+        zero_division=np.nan,
+        output_dict=True,
+    )
 
-    # Calculate metrics and similarity for video type groups
-    type_groups = video_df.groupby("actual_label")["feature_vector"].aggregate(lambda group: pdist(group.to_list(), metric="cosine").mean())
+    # Evaluation metrics and similarity for the whole set
+    general_eval_metrics = EvalMetricsModel(
+        precision=report["macro avg"]["precision"],
+        recall=report["macro avg"]["recall"],
+        f1_score=report["macro avg"]["f1-score"],
+    )
+    general_avg_cosine_distance = pdist(
+        video_df["feature_vector"].to_list(), metric="cosine"
+    ).mean()
 
-    for video_type in video_type_list:
-        eval_metrics = calc_eval_metrics(video_df.loc[video_df["actual_label"] == video_type], labels=video_type_list)
-        groups.append(GroupStatisticsModel(category=GroupCategoryEnum.topic, name=video_type, eval_metrics=eval_metrics, avg_cosine_intra_similarity=type_groups[video_type]))
+    groups.append(
+        GroupStatisticsModel(
+            name="all",
+            category=GroupCategoryEnum.all,
+            eval_metrics=general_eval_metrics,
+            avg_cosine_intra_similarity=general_avg_cosine_distance,
+        )
+    )
 
-    # Calculate metrics and similarity for tv show groups
-    tvshow_groups = video_df.groupby("tvshow")["feature_vector"].aggregate(lambda group: pdist(group.to_list(), metric="cosine").mean())
+    # Evaluation metrics and similarity for video type groups
+    type_groups = video_df.groupby("actual_label")["feature_vector"].aggregate(
+        lambda group: pdist(group.to_list(), metric="cosine").mean()
+    )
 
-    for tv_show in video_df["tvshow"].unique():
-        eval_metrics = calc_eval_metrics(video_df.loc[video_df["tvshow"] == tv_show], labels=video_type_list)
-        groups.append(GroupStatisticsModel(category=GroupCategoryEnum.tv_show, name=tv_show, eval_metrics=eval_metrics, avg_cosine_intra_similarity=tvshow_groups[tv_show]))
+    for video_topic in video_topic_list:
+        eval_metrics = EvalMetricsModel(
+            precision=report[video_topic]["precision"],
+            recall=report[video_topic]["recall"],
+            f1_score=report[video_topic]["f1-score"],
+        )
+        groups.append(
+            GroupStatisticsModel(
+                category=GroupCategoryEnum.topic,
+                name=video_topic,
+                eval_metrics=eval_metrics,
+                avg_cosine_intra_similarity=type_groups[video_topic],
+            )
+        )
+
+    # Evaluation metrics and similarity for tv show groups
+    tvshow_groups = video_df.groupby("tvshow")["feature_vector"].aggregate(
+        lambda group: pdist(group.to_list(), metric="cosine").mean()
+    )
+
+    # Add report metrics to dataframe
+    report_df = video_df.copy()
+    report_metrics = ["precision", "recall", "f1-score"]
+
+    for metric in report_metrics:
+        metric_map = {
+            label: report[label][metric] for label in report_df["actual_label"].unique()
+        }
+        report_df[metric] = report_df["actual_label"].map(metric_map)
+        report_df[f"tvshow_avg_{metric}"] = report_df.groupby("tvshow")[
+            metric
+        ].transform("mean")
+
+    for tv_show in report_df["tvshow"].unique():
+        example_record = report_df.loc[report_df["tvshow"] == tv_show].iloc[0]
+        eval_metrics = EvalMetricsModel(
+            precision=example_record["tvshow_avg_precision"],
+            recall=example_record["tvshow_avg_recall"],
+            f1_score=example_record["tvshow_avg_f1-score"],
+        )
+        groups.append(
+            GroupStatisticsModel(
+                category=GroupCategoryEnum.tv_show,
+                name=tv_show,
+                eval_metrics=eval_metrics,
+                avg_cosine_intra_similarity=tvshow_groups[tv_show],
+            )
+        )
 
     return groups
-    
-def calc_eval_metrics(video_df: pd.DataFrame, labels: list[str]) -> EvalMetricsModel:
-    """Calculates evaluation metrics for a given DataFrame.
-
-    Args:
-        video_df (pd.DataFrame): The video DataFrame.
-        labels (list[str]): The labels to consider.
-
-    Returns:
-        EvalMetricsModel: The precision, recall and f-1 score, macro-averaged.
-    """
-    precision = precision_score(video_df["actual_label"].values, video_df["predicted_label"].values, labels=labels, average="macro", zero_division=1.0)
-    recall = recall_score(video_df["actual_label"].values, video_df["predicted_label"].values, labels=labels, average="macro", zero_division=1.0)
-    f1 = f1_score(video_df["actual_label"].values, video_df["predicted_label"].values, labels=labels, average="macro", zero_division=1.0)
-
-    return EvalMetricsModel(precision=precision, recall=recall, f1_score=f1)